5933abdc7afe97ade0b7ed99ddfc7017ed771ebd
feti <iarison.michel@gmail.com>
2024-12-28 11:10:37 +0300

    Ajout du contrôleur UtilisateurController pour la gestion des utilisateurs

---
+++ src/main/java/controller/UtilisateurController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import dao.GenericDao;
+import modele.Utilisateur;
+
+public class UtilisateurController {
+    private GenericDao<Utilisateur> utilisateurDAO;
+
+    public UtilisateurController(GenericDao<Utilisateur> utilisateurDAO) {
+        this.utilisateurDAO = utilisateurDAO;
+    }
+
+    public void ajouterUtilisateur(Utilisateur utilisateur) {
+        utilisateurDAO.inserer(utilisateur);
+    }
+    
+    public void modifierUtilisateur(Utilisateur utilisateur){
+        utilisateurDAO.modifier(utilisateur);
+    }
+    
+    public void supprimerUtilisateur(int id){
+        utilisateurDAO.supprimer(id);
+    }
+}
